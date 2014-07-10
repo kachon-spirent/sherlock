c3e8a2fbe0693b3ca09608141e1f5f7e4d6bd179
ubuntu11-2 <kachon@ubuntu.(none)>
2014-07-10 12:57:07 -0700

    setup project

---
+++ dashboard/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dashboard::Application.config.secret_key_base = '97bc855f595f4de66634dc4ca52e2af0581678ba500f2494789c41157debd049351ae29940e521c67fd802d67475c8cc8775a3b3d5cea881df041148f37b5982'
