0481bde0991d394ae1cc0b74c8a51094c3a362f0
kalei-spirent <kahou.lei@spirent.com>
2014-07-10 14:36:21 -0700

    Setup traffic_center

---
+++ traffic_center/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrafficCenter::Application.config.secret_key_base = '5f462970a05b7ba7d2d2162dfc91a0f131f536428d9929838964bced2c53e17c461bbe38d654522f44b2fdf248831db1715479fcefee49e13c75fd7ba72a3b37'
---
+++ traffic_center/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TrafficCenter::Application.config.session_store :cookie_store, key: '_traffic_center_session'
